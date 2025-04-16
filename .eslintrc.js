ccbaf5957839e11b817c6f313c1828a43c6f0645
jinhok96 <jinhok96a@gmail.com>
2025-04-16 19:22:26 +0900

    chore: eslint 설정 업데이트 	modified:   .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,20 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'], // .jsx와 .tsx 모두 허용
+      },
+    ],
+    // ▼ React Native/TypeScript 특화 ▼
+    'react/jsx-uses-react': 'off', // React 17+에서는 필요 없음
+    'react/react-in-jsx-scope': 'off', // React 17+에서는 필요 없음
+    '@typescript-eslint/no-unused-vars': 'warn', // unused 변수를 error 대신 warn으로
+    // ▼ Airbnb 규칙 조정 ▼
+    'import/prefer-default-export': 'off', // named export 허용
+    'import/no-extraneous-dependencies': ['error', {devDependencies: true}], // devDependencies 허용
+    'react/require-default-props': 'off', // TypeScript에서는 불필요 (타입이 기본값 처리)
+    'react/jsx-props-no-spreading': 'off', // props spreading 허용 (HOC 등에서 유용)
   },
 };
