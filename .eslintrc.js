ca5140fae5ea15e2d116d0fa7465e58ce4f4533e
jinhok96 <jinhok96a@gmail.com>
2025-04-16 18:20:07 +0900

    chore: eslint, prettier 설정 업데이트, endOfLine 자동으로 설정 	modified:   .eslintrc.js 	modified:   .prettierrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
