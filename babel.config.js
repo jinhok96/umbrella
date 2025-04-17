cc95cac55795daf14287d0c6deddc9c4ef8aa9ac
Kang Jinho <59686080+jinhok96@users.noreply.github.com>
2025-04-17 17:26:00 +0900

    6-feature-project-structure (#14)
    * chore: 절대 경로 및 import 정렬 관련 패키지 설치, lint 스크립트 업데이트
    	modified:   package-lock.json
    	modified:   package.json
    
    * chore: 절대 경로, import 자동 정렬, 일부 eslint 및 prettier 설정 업데이트
    	modified:   .eslintrc.js
    	modified:   .prettierrc.js
    	modified:   babel.config.js
    	modified:   jest.config.js
    	modified:   tsconfig.json
    
    * chore: 폴더 구조 수정, Section 별도의 컴포넌트로 분리, 변경된 설정에 따라 lint 완료
    	modified:   index.js
    	modified:   metro.config.js
    	renamed:    App.tsx -> src/App.tsx
    	renamed:    __tests__/App.test.tsx -> src/__tests__/App.test.tsx
    	new file:   "src/assets/fonts/# \355\217\260\355\212\270"
    	new file:   "src/assets/images/# \354\235\264\353\257\270\354\247\200"
    	new file:   "src/components/# \354\236\254\354\202\254\354\232\251 UI"
    	new file:   src/components/Section.tsx
    	new file:   "src/hooks/# \354\273\244\354\212\244\355\205\200 \355\233\205"
    	new file:   "src/navigation/# \353\235\274\354\232\260\355\214\205"
    	new file:   "src/screens/# \355\216\230\354\235\264\354\247\200"
    	new file:   src/services/# API
    	new file:   src/store/# Zustand
    
    * chore: libs 폴더 추가
    	modified:   babel.config.js
    	modified:   jest.config.js
    	new file:   "src/libs/constants/# \352\263\265\355\206\265 \354\203\201\354\210\230 \353\260\217 \352\260\235\354\262\264"
    	new file:   "src/libs/utils/# \354\234\240\355\213\270 \355\225\250\354\210\230"
    	modified:   tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@libs': './src/libs',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@
  * @format
  */
 
-import type {PropsWithChildren} from 'react';
 import React from 'react';
 import {
   ScrollView,
@@ -15,7 +14,6 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
 import {
   Colors,
   DebugInstructions,
@@ -24,37 +22,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import Section from '@components/Section';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
+export default function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -79,7 +49,7 @@ function App(): React.JSX.Element {
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
+        <View style={{ paddingRight: safePadding }}>
           <Header />
         </View>
         <View
@@ -109,22 +79,7 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
   highlight: {
     fontWeight: '700',
   },
 });
-
-export default App;
