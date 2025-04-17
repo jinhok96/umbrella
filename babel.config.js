d177c90ea15e221a3f3305d132031037ccf36b08
jinhok96 <jinhok96a@gmail.com>
2025-04-17 17:09:14 +0900

    chore: 절대 경로, import 자동 정렬, 일부 eslint 및 prettier 설정 업데이트 	modified:   .eslintrc.js 	modified:   .prettierrc.js 	modified:   babel.config.js 	modified:   jest.config.js 	modified:   tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
