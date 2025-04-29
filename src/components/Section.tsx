227b433d7acde2f2168ab9fb75e00f4d39770237
jinhok96 <jinhok96a@gmail.com>
2025-04-29 18:23:44 +0900

    chore: 환경 변수 타입 추가 및 콘솔 출력 	modified:   src/components/Section.tsx 	new file:   src/libs/types/react-native-config-d.ts

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, useEffect } from 'react';
 import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import { useGetJsonPlaceholderTodo } from '@services/template/query';
+import Config from 'react-native-config';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -10,11 +10,13 @@ type SectionProps = PropsWithChildren<{
 
 export default function Section({ children, title }: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-  const { data } = useGetJsonPlaceholderTodo(1);
+  // const { data } = useGetJsonPlaceholderTodo(1);
 
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    // console.log(data);
+    console.log(Config.OPEN_WEATHER_API_BASE_URL);
+    console.log(Config.OPEN_WEATHER_API_KEY);
+  }, []);
 
   return (
     <View style={styles.sectionContainer}>
