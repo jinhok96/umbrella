65aa6c4d3033dbb966070156ee1309a16cdf9901
jinhok96 <jinhok96a@gmail.com>
2025-05-01 20:46:00 +0900

    chore: mock OPEN_WEATHER_API_BASE_URL 주소 변경 	modified:   src/__mocks__/react-native-config.js

--- src/__mocks__/react-native-config.js
+++ src/__mocks__/react-native-config.js
@@ -1,4 +1,4 @@
 export default {
-  OPEN_WEATHER_API_BASE_URL: 'OpenWeatherApiBaseUrlMock',
+  OPEN_WEATHER_API_BASE_URL: 'https://openweatherapibaseurlmock.com',
   OPEN_WEATHER_API_KEY: 'OpenWeatherApiKeyMock',
 };
