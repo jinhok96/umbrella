60b8891659a9e2dc8f72178a5f1836c407e9e35f
jinhok96 <jinhok96a@gmail.com>
2025-05-06 20:59:48 +0900

    chore: status.ts로 httpClient 테스트 코드 업데이트 	modified:   src/services/httpClient/__test__/httpClient.test.ts

--- src/services/httpClient/__test__/httpClient.test.ts
+++ src/services/httpClient/__test__/httpClient.test.ts
@@ -2,6 +2,7 @@ import nock from 'nock';
 
 import HttpClient from '@services/httpClient/httpClient';
 import { PickedAxiosResponse } from '@services/httpClient/httpClient.type';
+import { HTTP_CLIENT_STATUS_LIST } from '@services/httpClient/status';
 
 const BASE_URL = 'https://example.com';
 const httpClient = new HttpClient(BASE_URL);
@@ -12,32 +13,32 @@ describe('HttpClient', () => {
   });
 
   test('baseURL 비어있을 경우 에러 throw 테스트', () => {
-    expect(() => new HttpClient('')).toThrow('baseURL이 없습니다.');
+    expect(() => new HttpClient('')).toThrow(HTTP_CLIENT_STATUS_LIST.BASE_URL_MISSING_ERROR.statusText);
   });
 
   test('GET 요청 성공', async () => {
     const mockURL = '/get';
     const mockData = { id: 1, name: 'Test' };
-    nock(BASE_URL).get(mockURL).reply(200, mockData);
+    nock(BASE_URL).get(mockURL).reply(HTTP_CLIENT_STATUS_LIST.OK.status, mockData);
 
     const { data, status, statusText, headers } = await httpClient.get(mockURL);
     expect(data).toEqual(mockData);
-    expect(status).toBe(200);
-    expect(statusText).toBe('OK');
+    expect(status).toBe(HTTP_CLIENT_STATUS_LIST.OK.status);
+    expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.OK.statusText);
     expect(headers).toMatchObject({ 'content-type': 'application/json' });
   });
 
   test('GET 요청 오류', async () => {
     const mockURL = '/get';
-    nock(BASE_URL).get(mockURL).reply(500);
+    nock(BASE_URL).get(mockURL).reply(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
 
     try {
       await httpClient.get(mockURL);
     } catch (error) {
       const { data, status, statusText, headers } = error as PickedAxiosResponse<null>;
       expect(data).toBeNull();
-      expect(status).toBe(500);
-      expect(statusText).toBe('Internal Server Error');
+      expect(status).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
+      expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.statusText);
       expect(headers).toMatchObject({});
     }
   });
@@ -46,27 +47,27 @@ describe('HttpClient', () => {
     const mockURL = '/post';
     const mockData = { id: 1, name: 'Test' };
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).post(mockURL).reply(201, mockData);
+    nock(BASE_URL).post(mockURL).reply(HTTP_CLIENT_STATUS_LIST.CREATED.status, mockData);
 
     const { data, status, statusText, headers } = await httpClient.post(mockURL, mockPayload);
     expect(data).toEqual(mockData);
-    expect(status).toBe(201);
-    expect(statusText).toBe('Created');
+    expect(status).toBe(HTTP_CLIENT_STATUS_LIST.CREATED.status);
+    expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.CREATED.statusText);
     expect(headers).toMatchObject({ 'content-type': 'application/json' });
   });
 
   test('POST 요청 오류', async () => {
     const mockURL = '/post';
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).post(mockURL).reply(500);
+    nock(BASE_URL).post(mockURL).reply(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
 
     try {
       await httpClient.post(mockURL, mockPayload);
     } catch (error) {
       const { data, status, statusText, headers } = error as PickedAxiosResponse<null>;
       expect(data).toBeNull();
-      expect(status).toBe(500);
-      expect(statusText).toBe('Internal Server Error');
+      expect(status).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
+      expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.statusText);
       expect(headers).toMatchObject({});
     }
   });
@@ -75,27 +76,27 @@ describe('HttpClient', () => {
     const mockURL = '/put';
     const mockData = { id: 1, name: 'Test' };
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).put(mockURL).reply(200, mockData);
+    nock(BASE_URL).put(mockURL).reply(HTTP_CLIENT_STATUS_LIST.OK.status, mockData);
 
     const { data, status, statusText, headers } = await httpClient.put(mockURL, mockPayload);
     expect(data).toEqual(mockData);
-    expect(status).toBe(200);
-    expect(statusText).toBe('OK');
+    expect(status).toBe(HTTP_CLIENT_STATUS_LIST.OK.status);
+    expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.OK.statusText);
     expect(headers).toMatchObject({ 'content-type': 'application/json' });
   });
 
   test('PUT 요청 오류', async () => {
     const mockURL = '/put';
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).put(mockURL).reply(500);
+    nock(BASE_URL).put(mockURL).reply(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
 
     try {
       await httpClient.put(mockURL, mockPayload);
     } catch (error) {
       const { data, status, statusText, headers } = error as PickedAxiosResponse<null>;
       expect(data).toBeNull();
-      expect(status).toBe(500);
-      expect(statusText).toBe('Internal Server Error');
+      expect(status).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
+      expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.statusText);
       expect(headers).toMatchObject({});
     }
   });
@@ -104,27 +105,27 @@ describe('HttpClient', () => {
     const mockURL = '/patch';
     const mockData = { id: 1, name: 'Test' };
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).patch(mockURL).reply(200, mockData);
+    nock(BASE_URL).patch(mockURL).reply(HTTP_CLIENT_STATUS_LIST.OK.status, mockData);
 
     const { data, status, statusText, headers } = await httpClient.patch(mockURL, mockPayload);
     expect(data).toEqual(mockData);
-    expect(status).toBe(200);
-    expect(statusText).toBe('OK');
+    expect(status).toBe(HTTP_CLIENT_STATUS_LIST.OK.status);
+    expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.OK.statusText);
     expect(headers).toMatchObject({ 'content-type': 'application/json' });
   });
 
   test('PATCH 요청 오류', async () => {
     const mockURL = '/patch';
     const mockPayload = { name: 'Test' };
-    nock(BASE_URL).patch(mockURL).reply(500);
+    nock(BASE_URL).patch(mockURL).reply(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
 
     try {
       await httpClient.patch(mockURL, mockPayload);
     } catch (error) {
       const { data, status, statusText, headers } = error as PickedAxiosResponse<null>;
       expect(data).toBeNull();
-      expect(status).toBe(500);
-      expect(statusText).toBe('Internal Server Error');
+      expect(status).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
+      expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.statusText);
       expect(headers).toMatchObject({});
     }
   });
@@ -132,26 +133,26 @@ describe('HttpClient', () => {
   test('DELETE 요청 성공', async () => {
     const mockURL = '/delete';
     const mockData = { id: 1, name: 'Test' };
-    nock(BASE_URL).delete(mockURL).reply(200, mockData);
+    nock(BASE_URL).delete(mockURL).reply(HTTP_CLIENT_STATUS_LIST.OK.status, mockData);
 
     const { data, status, statusText, headers } = await httpClient.delete(mockURL);
     expect(data).toEqual(mockData);
-    expect(status).toBe(200);
-    expect(statusText).toBe('OK');
+    expect(status).toBe(HTTP_CLIENT_STATUS_LIST.OK.status);
+    expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.OK.statusText);
     expect(headers).toMatchObject({ 'content-type': 'application/json' });
   });
 
   test('DELETE 요청 오류', async () => {
     const mockURL = '/delete';
-    nock(BASE_URL).delete(mockURL).reply(500);
+    nock(BASE_URL).delete(mockURL).reply(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
 
     try {
       await httpClient.delete(mockURL);
     } catch (error) {
       const { data, status, statusText, headers } = error as PickedAxiosResponse<null>;
       expect(data).toBeNull();
-      expect(status).toBe(500);
-      expect(statusText).toBe('Internal Server Error');
+      expect(status).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.status);
+      expect(statusText).toBe(HTTP_CLIENT_STATUS_LIST.INTERNAL_SERVER_ERROR.statusText);
       expect(headers).toMatchObject({});
     }
   });
