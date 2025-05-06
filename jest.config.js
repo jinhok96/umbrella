6883acffaabc3fb2211010855fbb596cdfd9ba1e
jinhok96 <jinhok96a@gmail.com>
2025-05-06 20:55:13 +0900

    chore: 테스트 코드에서 외부 모듈 import를 위해 업데이트 	modified:   jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  // transformIgnorePatterns 필요할 때마다 모듈 추가
+  transformIgnorePatterns: ['node_modules/(?!react-native|react-navigation|react-native-error-boundary)/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['<rootDir>/index.js'],
   moduleNameMapper: {
