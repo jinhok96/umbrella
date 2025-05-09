72c4b2bc692aa8a2722f14275b6b50a1bf51b875
jinhok96 <jinhok96a@gmail.com>
2025-05-09 16:29:29 +0900

    chore: httpClient 에러 상태 가져오는 함수 추가 	modified:   src/services/httpClient/__test__/utils.test.ts 	modified:   src/services/httpClient/utils.ts

--- src/services/httpClient/__test__/utils.test.ts
+++ src/services/httpClient/__test__/utils.test.ts
@@ -1,22 +1,40 @@
 import { HTTP_CLIENT_STATUS_LIST } from '@services/httpClient/status';
-import { httpClientError } from '@services/httpClient/utils';
+import { getHttpClientStatusMessage, httpClientError } from '@services/httpClient/utils';
 
 import type { PickedAxiosResponse } from '@services/httpClient/httpClient.type';
+import type { HttpClientStatusList } from '@services/httpClient/status.type';
 
+/**
+ * getHttpClientStatusMessage 테스트
+ * 실패 테스트는 getLocalizedTextFromMap에서 이미 수행함
+ * @jinhok96 25.05.08
+ */
+describe('getHttpClientStatusMessage', () => {
+  test('성공 테스트', () => {
+    const status: keyof HttpClientStatusList = '9999';
+    expect(getHttpClientStatusMessage(status, 'en')).toBe(HTTP_CLIENT_STATUS_LIST[status].en);
+    expect(getHttpClientStatusMessage(status, 'kr')).toBe(HTTP_CLIENT_STATUS_LIST[status].kr);
+  });
+});
+
+/**
+ * httpClientError 테스트
+ * @jinhok96 25.05.08
+ */
 describe('httpClientError', () => {
   test('PickedAxiosResponse<T | null>를 반환하는지 테스트', () => {
-    const errorMock: PickedAxiosResponse<{ id: number }> = {
+    const mockError: PickedAxiosResponse<{ id: number }> = {
       data: { id: 1 },
       status: 401,
       statusText: 'Unauthorized',
       headers: { 'Content-Type': 'application/json' },
     };
-    const typedError = httpClientError(errorMock);
-    expect(typedError).toMatchObject(errorMock);
+    const typedError = httpClientError(mockError);
+    expect(typedError).toMatchObject(mockError);
   });
 
   test('HttpClient의 error가 아닐 경우 에러 throw 테스트', () => {
-    const errorMock = { message: 'error' };
-    expect(() => httpClientError(errorMock)).toThrow(HTTP_CLIENT_STATUS_LIST.INVALID_HTTP_CLIENT_ERROR.statusText);
+    const mockError = { message: 'error' };
+    expect(() => httpClientError(mockError)).toThrow(getHttpClientStatusMessage('10003'));
   });
 });
