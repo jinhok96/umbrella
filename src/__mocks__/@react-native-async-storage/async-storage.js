bdbc211489d97cb35c6702cb8aab5a9177d35ca0
jinhok96 <jinhok96a@gmail.com>
2025-05-12 20:34:27 +0900

    chore: async-storage.js 작동하지 않던 문제 수정 	modified:   src/__mocks__/@react-native-async-storage/async-storage.js

--- src/__mocks__/@react-native-async-storage/async-storage.js
+++ src/__mocks__/@react-native-async-storage/async-storage.js
@@ -1,1 +1,4 @@
-export * from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import AsyncStorage, { useAsyncStorage } from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+export default AsyncStorage;
+export { useAsyncStorage };
