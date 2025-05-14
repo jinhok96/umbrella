9c7ef56478f90538e14999c7cdf84e130aa8df0d
jinhok96 <jinhok96a@gmail.com>
2025-05-14 16:16:23 +0900

    chore: 조건부 렌더링을 선언적으로 처리하는 Show 컴포넌트 추가 	new file:   src/components/common/Show.tsx

---
+++ src/components/common/Show.tsx
@@ -0,0 +1,14 @@
+import type { PropsWithChildren, ReactNode } from 'react';
+
+type ShowProps = PropsWithChildren<{ when: boolean; fallback?: ReactNode }>;
+
+/**
+ * 조건부 렌더링을 선언적으로 처리하는 컴포넌트
+ * @param children 렌더링할 요소
+ * @param when 렌더링 조건
+ * @param fallback when=false일 경우 렌더링할 요소
+ * @jinhok96 25.05.14
+ */
+export default function Show({ children, when, fallback = null }: ShowProps) {
+  return when ? children : fallback;
+}
