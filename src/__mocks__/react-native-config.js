5d576fdf7ad13f065545173c632fde2edc98ba94
jinhok96 <jinhok96a@gmail.com>
2025-05-14 20:01:43 +0900

    chore: env 추가 	modified:   src/__mocks__/react-native-config.js 	modified:   src/libs/types/react-native-config.d.ts

--- src/__mocks__/react-native-config.js
+++ src/__mocks__/react-native-config.js
@@ -1,4 +1,5 @@
 export default {
   OPEN_WEATHER_API_BASE_URL: 'https://openweatherapibaseurlmock.com',
   OPEN_WEATHER_API_KEY: 'OpenWeatherApiKeyMock',
+  GOOGLE_MAPS_API_KEY: 'GoogleMapsApiKeyMock',
 };
--- src/libs/types/react-native-config.d.ts
+++ src/libs/types/react-native-config.d.ts
@@ -2,6 +2,7 @@ declare module 'react-native-config' {
   export interface NativeConfig {
     OPEN_WEATHER_API_BASE_URL?: string;
     OPEN_WEATHER_API_KEY?: string;
+    GOOGLE_MAPS_API_KEY?: string;
   }
 
   export const Config: NativeConfig;
