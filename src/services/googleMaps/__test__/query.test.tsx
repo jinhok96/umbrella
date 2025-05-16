a3e28cb9e4d105c861883e89fda42175a83f5a80
jinhok96 <jinhok96a@gmail.com>
2025-05-16 22:38:20 +0900

    chore: air quality api 훅 테스트 코드 추가 	modified:   src/services/googleMaps/__test__/query.test.tsx

--- src/services/googleMaps/__test__/query.test.tsx
+++ src/services/googleMaps/__test__/query.test.tsx
@@ -10,7 +10,13 @@ import ErrorBoundary from 'react-native-error-boundary';
 
 import { googleMapsService } from '@services/googleMaps/axios';
 import { GOOGLE_MAPS_SERVICE_MOCK } from '@services/googleMaps/mock/test.mock';
-import { useGetAutocompleteRegions, useGetPlaceGeocoding, useGetReverseGeocoding } from '@services/googleMaps/query';
+import {
+  useGetAirQualityHourlyForecasts,
+  useGetAutocompleteRegions,
+  useGetCurrentAirQuality,
+  useGetPlaceGeocoding,
+  useGetReverseGeocoding,
+} from '@services/googleMaps/query';
 
 // 서비스 모듈 모킹
 jest.mock('@services/googleMaps/axios', () => ({
@@ -18,6 +24,8 @@ jest.mock('@services/googleMaps/axios', () => ({
     postAutocompleteRegions: jest.fn(),
     getPlaceGeocoding: jest.fn(),
     getReverseGeocoding: jest.fn(),
+    postCurrentAirQuality: jest.fn(),
+    postAirQualityHourlyForecasts: jest.fn(),
   },
 }));
 
@@ -33,7 +41,7 @@ const wrapper = ({ children }: PropsWithChildren) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
 
-describe('templateService Hooks', () => {
+describe('googleMapsService Hooks - Places, Geocoding', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     queryClient.clear();
@@ -167,6 +175,116 @@ describe('templateService Hooks', () => {
 
       function TestComponent() {
         useHook(mock.PARAMS);
+        return null;
+      }
+
+      await act(async () => {
+        render(
+          <ErrorBoundary
+            FallbackComponent={error => <div>{error.error.message}</div>}
+            onError={error => {
+              expect(error).toBeInstanceOf(Error);
+              expect(error.message).toBe(errorMessageMock);
+            }}>
+            {wrapper({ children: <TestComponent /> })}
+          </ErrorBoundary>,
+        );
+      });
+
+      await waitFor(() => {
+        expect(screen.queryByText(errorMessageMock)?.textContent).toBe(errorMessageMock);
+      });
+    });
+  });
+});
+
+describe('googleMapsService Hooks - Air Quality', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    queryClient.clear();
+
+    // 터미널에 console.error 표시되지 않도록 console.error 모킹
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  const errorMessageMock = GOOGLE_MAPS_SERVICE_MOCK.HTTP_CLIENT_AIR_QUALITY_ERROR.statusText;
+
+  /**
+   * useGetCurrentAirQuality 테스트
+   * @jinhok96 25.05.16
+   */
+  describe('useGetCurrentAirQuality', () => {
+    const service = googleMapsService.postCurrentAirQuality as jest.Mock;
+    const useHook = useGetCurrentAirQuality;
+    const mock = GOOGLE_MAPS_SERVICE_MOCK.POST_CURRENT_AIR_QUALITY;
+
+    test('API 응답 성공', async () => {
+      service.mockResolvedValue(mock.RESPONSE);
+
+      const { result } = renderHook(() => useHook(mock.PAYLOAD), { wrapper });
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBe(true);
+        expect(result.current?.data).toEqual(mock.RESPONSE);
+      });
+    });
+
+    test('에러 throw 테스트', async () => {
+      service.mockRejectedValue(new Error(errorMessageMock));
+
+      function TestComponent() {
+        useHook(mock.PAYLOAD);
+        return null;
+      }
+
+      await act(async () => {
+        render(
+          <ErrorBoundary
+            FallbackComponent={error => <div>{error.error.message}</div>}
+            onError={error => {
+              expect(error).toBeInstanceOf(Error);
+              expect(error.message).toBe(errorMessageMock);
+            }}>
+            {wrapper({ children: <TestComponent /> })}
+          </ErrorBoundary>,
+        );
+      });
+
+      await waitFor(() => {
+        expect(screen.queryByText(errorMessageMock)?.textContent).toBe(errorMessageMock);
+      });
+    });
+  });
+
+  /**
+   * useGetCurrentAirQuality 테스트
+   * @jinhok96 25.05.16
+   */
+  describe('useGetAirQualityHourlyForecasts', () => {
+    const service = googleMapsService.postAirQualityHourlyForecasts as jest.Mock;
+    const useHook = useGetAirQualityHourlyForecasts;
+    const mock = GOOGLE_MAPS_SERVICE_MOCK.POST_AIR_QUALITY_HOURLY_FORECASTS;
+
+    test('API 응답 성공', async () => {
+      service.mockResolvedValue(mock.RESPONSE);
+
+      const { result } = renderHook(() => useHook(mock.PAYLOAD), { wrapper });
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBe(true);
+        expect(result.current?.data).toEqual(mock.RESPONSE);
+      });
+    });
+
+    test('에러 throw 테스트', async () => {
+      service.mockRejectedValue(new Error(errorMessageMock));
+
+      function TestComponent() {
+        useHook(mock.PAYLOAD);
         return null;
       }
 
