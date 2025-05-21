0d41b435bd5234383218c955859e5c748c9a923e
jinhok96 <jinhok96a@gmail.com>
2025-05-21 22:37:58 +0900

    chore: 바뀐 FontText 반영 	modified:   src/components/text/MontserratText.tsx 	modified:   src/components/text/PretendardText.tsx

--- src/components/text/PretendardText.tsx
+++ src/components/text/PretendardText.tsx
@@ -1,13 +1,13 @@
 import FontText from '@components/text/FontText';
 
-import type { FontTextProps } from '@components/text/FontText.type';
+import type { FontTextProps, PretendardTypography } from '@components/text/FontText.type';
 
-type PretendardTextProps = Omit<FontTextProps, 'font'>;
+type PretendardTextProps = Omit<FontTextProps<PretendardTypography>, 'font'>;
 
 /**
  * `font-pretendard-${weight}`를 적용하는 커스텀 Text 컴포넌트
  * @param className Text에 전달할 className
- * @param weight 폰트 굵기 (기본값: `regular`) `regular | medium | semibold | bold`
+ * @param typo 타이포그래피
  * @returns `Text` 컴포넌트
  * @jinhok96 25.05.21
  */
