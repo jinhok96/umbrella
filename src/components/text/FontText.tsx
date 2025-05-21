11e860e163b884fead53e95634e7e4c7766e1c21
jinhok96 <jinhok96a@gmail.com>
2025-05-21 22:36:25 +0900

    chore: 폰트별 타이포그래피 스타일 분리, 폰트, 무게, 타이포 모두 결합 	modified:   src/components/text/FontText.tsx 	modified:   src/components/text/FontText.type.tsx

--- src/components/text/FontText.tsx
+++ src/components/text/FontText.tsx
@@ -1,19 +1,51 @@
 import { Text } from 'react-native';
 
-import type { FontTextProps } from '@components/text/FontText.type';
+import type {
+  FontTextProps,
+  MontserratTypography,
+  PretendardTypography,
+  Typography,
+} from '@components/text/FontText.type';
 
-const FONT_CLASS_NAME_LIST: Record<FontTextProps['font'], Record<NonNullable<FontTextProps['weight']>, string>> = {
+type FontClassNameListType = {
+  pretendard: Record<FontTextProps<PretendardTypography>['typo'], string>;
+  montserrat: Record<FontTextProps<MontserratTypography>['typo'], string>;
+};
+
+const FONT_CLASS_NAME_LIST: FontClassNameListType = {
   pretendard: {
-    regular: 'font-pretendard-regular',
-    medium: 'font-pretendard-medium',
-    semibold: 'font-pretendard-semibold',
-    bold: 'font-pretendard-bold',
+    'title-1': 'font-pretendard-bold text-pretendard-title-1',
+    'title-2': 'font-pretendard-bold text-pretendard-title-2',
+    'title-3': 'font-pretendard-bold text-pretendard-title-3',
+    'title-4': 'font-pretendard-bold text-pretendard-title-4',
+    'title-5': 'font-pretendard-bold text-pretendard-title-5',
+    'body-1': 'font-pretendard-regular text-pretendard-body-1',
+    'body-2': 'font-pretendard-regular text-pretendard-body-2',
+    'body-3': 'font-pretendard-regular text-pretendard-body-3',
+    'body-4': 'font-pretendard-regular text-pretendard-body-4',
+    'caption-1': 'font-pretendard-medium text-pretendard-caption-1',
+    'caption-2': 'font-pretendard-medium text-pretendard-caption-2',
+    'caption-3': 'font-pretendard-medium text-pretendard-caption-3',
+    'caption-4': 'font-pretendard-medium text-pretendard-caption-4',
+    'button-1': 'font-pretendard-semibold text-pretendard-button-1',
+    'button-2': 'font-pretendard-semibold text-pretendard-button-2',
   },
   montserrat: {
-    regular: 'font-montserrat-regular',
-    medium: 'font-montserrat-medium',
-    semibold: 'font-montserrat-semibold',
-    bold: 'font-montserrat-bold',
+    'title-1': 'font-montserrat-bold text-montserrat-title-1',
+    'title-2': 'font-montserrat-bold text-montserrat-title-2',
+    'title-3': 'font-montserrat-bold text-montserrat-title-3',
+    'title-4': 'font-montserrat-bold text-montserrat-title-4',
+    'title-5': 'font-montserrat-bold text-montserrat-title-5',
+    'body-1': 'font-montserrat-regular text-montserrat-body-1',
+    'body-2': 'font-montserrat-regular text-montserrat-body-2',
+    'body-3': 'font-montserrat-regular text-montserrat-body-3',
+    'body-4': 'font-montserrat-regular text-montserrat-body-4',
+    'caption-1': 'font-montserrat-medium text-montserrat-caption-1',
+    'caption-2': 'font-montserrat-medium text-montserrat-caption-2',
+    'caption-3': 'font-montserrat-medium text-montserrat-caption-3',
+    'caption-4': 'font-montserrat-medium text-montserrat-caption-4',
+    'button-1': 'font-montserrat-semibold text-montserrat-button-1',
+    'button-2': 'font-montserrat-semibold text-montserrat-button-2',
   },
 };
 
@@ -21,11 +53,11 @@ const FONT_CLASS_NAME_LIST: Record<FontTextProps['font'], Record<NonNullable<Fon
  * `font-${font}-${weight}`를 적용하는 커스텀 Text 컴포넌트
  * @param className Text에 전달할 className
  * @param font 폰트 종류 `pretendard | montserrat`
- * @param weight 폰트 굵기 (기본값: `regular`) `regular | medium | semibold | bold`
+ * @param typo 타이포그래피
  * @returns `Text` 컴포넌트
  * @jinhok96 25.05.21
  */
-export default function FontText({ children, className = '', font, weight = 'regular' }: FontTextProps) {
-  const fontClassName = FONT_CLASS_NAME_LIST[font][weight];
+export default function FontText<T extends Typography>({ children, className = '', font, typo }: FontTextProps<T>) {
+  const fontClassName = FONT_CLASS_NAME_LIST[font][typo];
   return <Text className={`${className} ${fontClassName}`}>{children}</Text>;
 }
--- src/components/text/FontText.type.tsx
+++ src/components/text/FontText.type.tsx
@@ -1,10 +1,53 @@
 import type { PropsWithChildren } from 'react';
 
 type Font = 'pretendard' | 'montserrat';
-type Weight = 'regular' | 'medium' | 'semibold' | 'bold';
 
-export type FontTextProps = PropsWithChildren<{
+export type PretendardTypography =
+  // Title
+  | 'title-1'
+  | 'title-2'
+  | 'title-3'
+  | 'title-4'
+  | 'title-5'
+  // Body
+  | 'body-1'
+  | 'body-2'
+  | 'body-3'
+  | 'body-4'
+  // Caption
+  | 'caption-1'
+  | 'caption-2'
+  | 'caption-3'
+  | 'caption-4'
+  // Button
+  | 'button-1'
+  | 'button-2';
+
+export type MontserratTypography =
+  // Title
+  | 'title-1'
+  | 'title-2'
+  | 'title-3'
+  | 'title-4'
+  | 'title-5'
+  // Body
+  | 'body-1'
+  | 'body-2'
+  | 'body-3'
+  | 'body-4'
+  // Caption
+  | 'caption-1'
+  | 'caption-2'
+  | 'caption-3'
+  | 'caption-4'
+  // Button
+  | 'button-1'
+  | 'button-2';
+
+export type Typography = PretendardTypography | MontserratTypography;
+
+export type FontTextProps<T extends Typography> = PropsWithChildren<{
   className?: string;
   font: Font;
-  weight?: Weight;
+  typo: T;
 }>;
