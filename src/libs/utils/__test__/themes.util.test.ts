49379c25a49e21eea9987349ceb0bf6a8bbed7d1
jinhok96 <jinhok96a@gmail.com>
2025-05-25 19:23:07 +0900

    chore: 컬러 토큰 업데이트 	modified:   src/libs/utils/__test__/themes.util.test.ts 	modified:   src/libs/utils/themes.type.ts 	modified:   src/libs/utils/themes.util.ts 	modified:   tailwind.config.ts

--- src/libs/utils/__test__/themes.util.test.ts
+++ src/libs/utils/__test__/themes.util.test.ts
@@ -1,11 +1,9 @@
 import { colorThemeVarList } from '@libs/utils/themes.util';
 
-import type { ColorTheme } from '@libs/utils/themes.type';
-
 describe('colorThemeVarList', () => {
   test('모든 colorTheme 키에 --color- 접두사가 붙는지 확인', () => {
     // 모든 테마 키를 추출 (light, dark, highContrast 등)
-    const themeKeys = Object.keys(colorThemeVarList) as Array<keyof ColorTheme>;
+    const themeKeys = Object.keys(colorThemeVarList) as Array<keyof typeof colorThemeVarList>;
 
     themeKeys.forEach(themeKey => {
       const theme = colorThemeVarList[themeKey];
