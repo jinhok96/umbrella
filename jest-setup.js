fa713de3771359093dbc242e0d6b09cc563ee74f
jinhok96 <jinhok96a@gmail.com>
2025-05-25 17:32:33 +0900

    chore: react-navigation을 사용하기 위해 jest, tsconfig, android 설정 	modified:   android/app/src/main/java/com/umbrella/MainActivity.kt 	modified:   jest-setup.js 	modified:   jest.polyfills.js 	modified:   tsconfig.json

--- jest-setup.js
+++ jest-setup.js
@@ -1,2 +1,7 @@
 import '@testing-library/jest-dom';
 import 'whatwg-fetch';
+
+import { setUpTests } from 'react-native-reanimated';
+
+// Include this section for mocking react-native-reanimated
+setUpTests();
