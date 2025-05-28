8ec44280210df8446e578f13121249146d6e28e0
jinhok96 <jinhok96a@gmail.com>
2025-05-29 02:08:26 +0900

    chore: 라우터 상태를 저장하는 스토어 추가 	new file:   src/store/routeStore/__test__/useRouteStore.test.ts 	new file:   src/store/routeStore/useRouteStore.const.ts 	new file:   src/store/routeStore/useRouteStore.ts 	new file:   src/store/routeStore/useRouteStore.type.ts

---
+++ src/store/routeStore/__test__/useRouteStore.test.ts
@@ -0,0 +1,82 @@
+import { act, renderHook } from '@testing-library/react-native';
+
+import { routeStore, useRouteStore } from '@store/routeStore/useRouteStore';
+import { INIT_ROUTE_STORE_STATE } from '@store/routeStore/useRouteStore.const';
+
+import type { RouteStoreState } from '@store/routeStore/useRouteStore.type';
+
+type StoreState = RouteStoreState;
+
+const INIT_STATE_MOCK: StoreState = {
+  isReady: false,
+  currentRouteName: undefined,
+};
+
+const NEW_STATE_MOCK: StoreState = {
+  isReady: true,
+  currentRouteName: 'Home',
+};
+
+/**
+ * useRouteStore 테스트
+ * @jinhok96 25.05.29
+ */
+describe('useRouteStore', () => {
+  const store = routeStore;
+  const useStore = useRouteStore;
+
+  beforeEach(() => {
+    // 각 테스트 전에 스토어를 초기 상태로 리셋
+    store.setState(INIT_STATE_MOCK);
+  });
+
+  afterAll(() => {
+    // 모든 테스트 완료 후 스토어를 초기 상태로 리셋
+    store.setState(INIT_ROUTE_STORE_STATE);
+  });
+
+  test('초기 상태 확인', () => {
+    const { result } = renderHook(() => useStore());
+    expect(result.current).toMatchObject(INIT_STATE_MOCK);
+  });
+
+  test('훅을 사용하지 않고 직접 스토어 접근 테스트', () => {
+    expect(store.getState()).toMatchObject(INIT_STATE_MOCK);
+
+    const newState = NEW_STATE_MOCK.isReady;
+
+    act(() => {
+      store.getState().setIsReady(newState);
+    });
+
+    expect(store.getState().isReady).toBe(newState);
+  });
+
+  test('액션: setIsReady', () => {
+    const { result } = renderHook(() => useStore());
+    expect(result.current).toMatchObject(INIT_STATE_MOCK);
+
+    const newState = NEW_STATE_MOCK.isReady;
+    const newResult: StoreState = { ...INIT_STATE_MOCK, isReady: newState };
+
+    act(() => {
+      result.current.setIsReady(newState);
+    });
+
+    expect(result.current).toMatchObject(newResult);
+  });
+
+  test('액션: setCurrentRouteName', () => {
+    const { result } = renderHook(() => useStore());
+    expect(result.current).toMatchObject(INIT_STATE_MOCK);
+
+    const newState = NEW_STATE_MOCK.currentRouteName;
+    const newResult: StoreState = { ...INIT_STATE_MOCK, currentRouteName: newState };
+
+    act(() => {
+      result.current.setCurrentRouteName(newState);
+    });
+
+    expect(result.current).toMatchObject(newResult);
+  });
+});
---
+++ src/store/routeStore/useRouteStore.type.ts
@@ -0,0 +1,19 @@
+import type { RouteName } from '@navigation/Navigation.type';
+
+/**
+ * 앱 라우트 상태 스토어
+ * @ isReady - 라우트가 준비되었는지 여부
+ * @ currentRouteName - 현재 라우트 이름
+ * @jinhok96 25.05.29
+ */
+export type RouteStoreState = {
+  isReady: boolean;
+  currentRouteName?: RouteName;
+};
+
+export type RouteStoreActions = {
+  setIsReady: (state: RouteStoreState['isReady']) => void;
+  setCurrentRouteName: (state: RouteStoreState['currentRouteName']) => void;
+};
+
+export type RouteStore = RouteStoreState & RouteStoreActions;
