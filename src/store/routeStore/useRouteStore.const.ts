b78f790a46410954549c02b4539b04c281b0a0ef
jinhok96 <jinhok96a@gmail.com>
2025-05-29 21:40:14 +0900

    chore: 현재 라우트 상태를 저장하는 스토어 추가 	new file:   src/store/routeStore/__test__/useRouteStore.test.ts 	new file:   src/store/routeStore/useRouteStore.const.ts 	new file:   src/store/routeStore/useRouteStore.ts 	new file:   src/store/routeStore/useRouteStore.type.ts

---
+++ src/store/routeStore/useRouteStore.const.ts
@@ -0,0 +1,6 @@
+import type { RouteStoreState } from '@store/routeStore/useRouteStore.type';
+
+export const INIT_ROUTE_STORE_STATE: RouteStoreState = {
+  isReady: false,
+  currentRouteName: undefined,
+};
