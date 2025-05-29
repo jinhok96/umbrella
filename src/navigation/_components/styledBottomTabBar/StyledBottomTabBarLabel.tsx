f9a64c95406a5ba4bcc8a623396077cffcb966c4
jinhok96 <jinhok96a@gmail.com>
2025-05-29 20:56:28 +0900

    chore: 코드 정리 	modified:   src/navigation/_components/styledBottomTabBar/StyledBottomTabBarLabel.tsx

--- src/navigation/_components/styledBottomTabBar/StyledBottomTabBarLabel.tsx
+++ src/navigation/_components/styledBottomTabBar/StyledBottomTabBarLabel.tsx
@@ -15,18 +15,21 @@ type StyledBottomTabBarLabelProps = Omit<PretendardTextProps, 'className' | 'typ
  * 스타일이 적용된 `BottomTabBar` 라벨 컴포넌트
  * @param text 라벨 텍스트
  * @param focused 현재 선택된 탭인지 여부
- * @jinhok96 25.05.28
+ * @jinhok96 25.05.29
  */
 export default function StyledBottomTabBarLabel({ text, focused, ...props }: StyledBottomTabBarLabelProps) {
   const lang = useSettingStore(state => state.lang);
 
-  const textColorClassName = classNames(focused && 'text-morning', !focused && 'text-text-06');
+  const textColorClassName = classNames('w-full pt-1 text-center', {
+    'text-morning': focused,
+    'text-text-06': !focused,
+  });
 
   return (
     <PretendardText
       {...props}
       typo="caption-3"
-      className={`w-full pt-1 text-center ${textColorClassName}`}
+      className={textColorClassName}
     >
       {text[lang]}
     </PretendardText>
