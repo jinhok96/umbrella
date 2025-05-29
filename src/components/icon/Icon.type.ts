a97a75698b8dacf7e9f7a06db47c5ee7548412ad
jinhok96 <jinhok96a@gmail.com>
2025-05-30 06:55:23 +0900

    chore: 아이콘 타입에서 className 제거 	modified:   src/components/icon/Icon.type.ts

--- src/components/icon/Icon.type.ts
+++ src/components/icon/Icon.type.ts
@@ -3,6 +3,6 @@ import type { SvgProps } from 'react-native-svg';
 import type { ColorVar } from '@libs/utils/themes.type';
 
 export type IconProps<T extends Record<string, unknown> = {}> = T &
-  Omit<SvgProps, 'color'> & {
+  Omit<SvgProps, 'color' | 'className'> & {
     color?: ColorVar;
   };
