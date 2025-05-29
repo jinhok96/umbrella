70bbbfb2adfa91b26635d66cdb2f9de772beadef
jinhok96 <jinhok96a@gmail.com>
2025-05-30 06:53:03 +0900

    chore: 아이콘 업데이트 	modified:   src/components/icon/PlusIcon.tsx

--- src/components/icon/PlusIcon.tsx
+++ src/components/icon/PlusIcon.tsx
@@ -1,5 +1,3 @@
-import { View } from 'react-native';
-
 import PlusSvg from '@assets/svg/Plus.svg';
 import { getColorHex } from '@libs/utils/getColorHex.util';
 import { useSettingStore } from '@store/settingStore/useSettingStore';
@@ -9,11 +7,9 @@ import type { IconProps } from '@components/icon/Icon.type';
 export default function PlusIcon({ color = '--color-text-01', ...props }: IconProps) {
   const theme = useSettingStore(state => state.theme);
   return (
-    <View className="p-0.5">
-      <PlusSvg
-        {...props}
-        color={getColorHex(theme, color)}
-      />
-    </View>
+    <PlusSvg
+      {...props}
+      color={getColorHex(theme, color)}
+    />
   );
 }
