948474e0ce8eb45a3eb7826ee5802881b01762d6
jinhok96 <jinhok96a@gmail.com>
2025-06-03 23:13:14 +0900

    chore: 크기 조절 	modified:   src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx 	modified:   src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.const.tsx

--- src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx
+++ src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx
@@ -3,6 +3,7 @@ import { Pressable, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import Animated, { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import TestSection from '@components/TestSection';
 import { HOME_NAVIGATION_TEST_ID_LIST } from '@navigation/home/HomeNavigation.const';
@@ -12,6 +13,9 @@ import { MAX_WEATHER_HEADER_HEIGHT } from '@screens/HomeScreen/_components/weath
 
 export default function CurrentForecastScreen() {
   const { navigate } = useNavigation();
+  const { top } = useSafeAreaInsets();
+
+  console.log('top', top);
 
   // 현재 스크롤의 위치
   const scrollValue = useSharedValue(0);
@@ -30,7 +34,7 @@ export default function CurrentForecastScreen() {
         <Animated.ScrollView
           className="flex-1"
           onScroll={handleScroll}
-          style={{ paddingTop: MAX_WEATHER_HEADER_HEIGHT }}
+          style={{ paddingTop: MAX_WEATHER_HEADER_HEIGHT + top }}
         >
           <View className="flex-1 p-5">
             <Text>CurrentForecastScreen</Text>
