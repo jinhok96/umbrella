331642a8a6c6f139510e3b9fdbc3740792095272
Kang Jinho <59686080+jinhok96@users.noreply.github.com>
2025-06-05 15:05:48 +0900

    100-feature-remove-weather-info-header-animation (#101)
    ## 1. 이슈 번호
    
    - close #100
    
    ## 2. 작업 내용
    
    - WeatherInfoHeader 애니메이션 제거
    
    ## 3. 참고 자료 (필요 시 / 스크린샷, 동영상, ...)

--- src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx
+++ src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx
@@ -1,69 +1,44 @@
-import { Pressable, Text, View } from 'react-native';
+import { Pressable, ScrollView, Text, View } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
-
-import Animated, { clamp, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import TestSection from '@components/TestSection';
 import { HOME_NAVIGATION_TEST_ID_LIST } from '@navigation/home/HomeNavigation.const';
 import HomeScreenWrapper from '@screens/HomeScreen/_components/HomeScreenWrapper';
 import WeatherInfoHeader from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader';
-import {
-  MAX_SCROLL_VALUE,
-  WEATHER_HEADER_HEIGHT_SCALE,
-} from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.const';
 
 export default function CurrentForecastScreen() {
   const { navigate } = useNavigation();
-  const { top } = useSafeAreaInsets();
-
-  // 현재 스크롤의 위치
-  const scrollValue = useSharedValue(0);
-
-  // 스크롤 이벤트에 따라 scrollValue 값을 업데이트
-  const handleScroll = useAnimatedScrollHandler({
-    onScroll: e => {
-      scrollValue.value = clamp(e.contentOffset.y, 0, MAX_SCROLL_VALUE);
-    },
-  });
 
   return (
     <View className="relative flex-1">
-      <WeatherInfoHeader scrollValue={scrollValue} />
+      <WeatherInfoHeader />
       <HomeScreenWrapper testID={HOME_NAVIGATION_TEST_ID_LIST.CurrentForecast}>
-        <Animated.ScrollView
-          className="flex-1"
-          onScroll={handleScroll}
-        >
-          <View style={{ paddingTop: WEATHER_HEADER_HEIGHT_SCALE + top }}>
-            <View className="flex-1 p-5">
-              <Text>CurrentForecastScreen</Text>
-              <TestSection />
-              <Pressable
-                onPress={() => {
-                  navigate('Home');
-                }}
-              >
-                <Text>CurrentForecastScreen</Text>
-              </Pressable>
-              <Pressable
-                onPress={() => {
-                  navigate('Location');
-                }}
-              >
-                <Text>LocationScreen</Text>
-              </Pressable>
-              <Pressable
-                onPress={() => {
-                  navigate('Setting');
-                }}
-              >
-                <Text>SettingScreen</Text>
-              </Pressable>
-            </View>
-          </View>
-        </Animated.ScrollView>
+        <ScrollView className="flex-1 p-5">
+          <Text>CurrentForecastScreen</Text>
+          <TestSection />
+          <Pressable
+            onPress={() => {
+              navigate('Home');
+            }}
+          >
+            <Text>CurrentForecastScreen</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              navigate('Location');
+            }}
+          >
+            <Text>LocationScreen</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              navigate('Setting');
+            }}
+          >
+            <Text>SettingScreen</Text>
+          </Pressable>
+        </ScrollView>
       </HomeScreenWrapper>
     </View>
   );
