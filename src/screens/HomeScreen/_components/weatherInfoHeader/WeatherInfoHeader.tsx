205209646212cbb603fde43e9d93dbb52260eae4
jinhok96 <jinhok96a@gmail.com>
2025-06-05 15:00:44 +0900

    chore: 애니메이션 제거 	modified:   src/screens/HomeScreen/CurrentForecastScreen/CurrentForecastScreen.tsx 	modified:   src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.tsx 	modified:   src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderIcon.tsx 	modified:   src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderSummary.tsx 	modified:   src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderTempSection.tsx

--- src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.tsx
+++ src/screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.tsx
@@ -1,81 +1,33 @@
-import { useState } from 'react';
 import type { ViewProps } from 'react-native';
 import { View } from 'react-native';
 
-import type { SharedValue } from 'react-native-reanimated';
-import Animated, { interpolate, useAnimatedStyle, withTiming } from 'react-native-reanimated';
-
 import LocationHeader from '@screens/HomeScreen/_components/weatherInfoHeader/LocationHeader';
-import {
-  WEATHER_HEADER_HEIGHT,
-  WEATHER_HEADER_HEIGHT_SCALE,
-  WEATHER_HEADER_ICON_SIZE_SCALE,
-  WEATHER_INFO_HEADER_ANIMATION_DURATION,
-} from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeader.const';
 import WeatherInfoHeaderIcon from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderIcon';
 import WeatherInfoHeaderSummary from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderSummary';
 import WeatherInfoHeaderTempSection from '@screens/HomeScreen/_components/weatherInfoHeader/WeatherInfoHeaderTempSection';
 
-type WeatherInfoHeaderProps = Omit<ViewProps, 'className'> & {
-  scrollValue: SharedValue<number>;
-};
+type WeatherInfoHeaderProps = Omit<ViewProps, 'className'>;
 
 /**
  * 현재 위치 날씨 정보를 보여주는 컴포넌트
- * @jinhok96 25.06.04
+ * @jinhok96 25.06.05
  */
-export default function WeatherInfoHeader({ scrollValue, ...props }: WeatherInfoHeaderProps) {
-  const [containerWidth, setContainerWidth] = useState(1);
-
-  const MAX_SCROLL_VALUE = WEATHER_HEADER_HEIGHT_SCALE - WEATHER_HEADER_HEIGHT;
-
-  // Temp Container Style
-  const animatedTempContainerStyle = useAnimatedStyle(() => {
-    const paddingTop = interpolate(
-      scrollValue.value,
-      [0, MAX_SCROLL_VALUE],
-      [WEATHER_HEADER_ICON_SIZE_SCALE, 0],
-      'clamp',
-    );
-
-    return {
-      paddingTop: withTiming(paddingTop, {
-        duration: WEATHER_INFO_HEADER_ANIMATION_DURATION,
-      }),
-    };
-  });
-
+export default function WeatherInfoHeader({ ...props }: WeatherInfoHeaderProps) {
   return (
     <View
       {...props}
-      className="pt-safe absolute top-0 z-10 bg-morning transition-[max-height]"
+      className="pt-safe bg-morning transition-[max-height]"
     >
       <LocationHeader />
-      <View className="px-5 pb-7 pt-5">
-        <Animated.View
-          className="relative flex gap-2"
-          style={animatedTempContainerStyle}
-          onLayout={e => {
-            const newWidth = e.nativeEvent.layout.width;
-            if (newWidth) setContainerWidth(newWidth);
-          }}
-        >
-          {/* 날씨 아이콘 */}
-          <WeatherInfoHeaderIcon
-            scrollValue={scrollValue}
-            containerWidth={containerWidth}
-          />
+      <View className="flex flex-row justify-between gap-2 px-5 pb-7 pt-5">
+        <View className="flex gap-1">
           {/* 기온 */}
-          <WeatherInfoHeaderTempSection
-            scrollValue={scrollValue}
-            containerWidth={containerWidth}
-          />
+          <WeatherInfoHeaderTempSection />
           {/* 요약 */}
-          <WeatherInfoHeaderSummary
-            scrollValue={scrollValue}
-            containerWidth={containerWidth}
-          />
-        </Animated.View>
+          <WeatherInfoHeaderSummary />
+        </View>
+        {/* 날씨 아이콘 */}
+        <WeatherInfoHeaderIcon />
       </View>
     </View>
   );
