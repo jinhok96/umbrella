6ce765bfe65d215aaf055d1efdb0bd69b4e54cf2
jinhok96 <jinhok96a@gmail.com>
2025-06-06 15:19:56 +0900

    chore: 무드 아이콘 추가 	new file:   src/components/icon/MoodIcon.tsx

---
+++ src/components/icon/MoodIcon.tsx
@@ -0,0 +1,40 @@
+import MoodBadSvg from '@assets/svg/MoodBad.svg';
+import MoodGoodSvg from '@assets/svg/MoodGood.svg';
+import MoodNormalSvg from '@assets/svg/MoodNormal.svg';
+import Show from '@components/wrapper/Show';
+import { useGetColorHex } from '@hooks/useGetColorHex';
+
+import type { IconProps } from '@components/icon/Icon.type';
+
+type MoodIconProps = Omit<IconProps, 'color'> & {
+  type: 'good' | 'normal' | 'bad';
+};
+
+export default function MoodIcon({ type, ...props }: MoodIconProps) {
+  const goodColor = useGetColorHex('--color-morning');
+  const normalColor = useGetColorHex('--color-success');
+  const badColor = useGetColorHex('--color-error');
+
+  return (
+    <>
+      <Show when={type === 'good'}>
+        <MoodGoodSvg
+          {...props}
+          color={goodColor}
+        />
+      </Show>
+      <Show when={type === 'normal'}>
+        <MoodNormalSvg
+          {...props}
+          color={normalColor}
+        />
+      </Show>
+      <Show when={type === 'bad'}>
+        <MoodBadSvg
+          {...props}
+          color={badColor}
+        />
+      </Show>
+    </>
+  );
+}
