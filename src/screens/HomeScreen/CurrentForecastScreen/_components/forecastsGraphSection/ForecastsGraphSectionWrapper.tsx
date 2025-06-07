197b235b5dc47ad1e3294e9cc35a584f3ffce3cd
jinhok96 <jinhok96a@gmail.com>
2025-06-07 22:34:30 +0900

    chore: 좌우 그라디언트 추가 	modified:   src/navigation/_components/styledHeader/StyledHeaderTitle.tsx 	modified:   src/screens/HomeScreen/CurrentForecastScreen/_components/forecastsGraphSection/ForecastsGraphSectionWrapper.tsx

--- src/screens/HomeScreen/CurrentForecastScreen/_components/forecastsGraphSection/ForecastsGraphSectionWrapper.tsx
+++ src/screens/HomeScreen/CurrentForecastScreen/_components/forecastsGraphSection/ForecastsGraphSectionWrapper.tsx
@@ -1,13 +1,39 @@
 import { useEffect, useRef, useState } from 'react';
 import { ScrollView, View } from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 import Show from '@components/wrapper/Show';
+import { useGetColorHex } from '@hooks/useGetColorHex';
 import { FORECASTS_GRAPH_SPACING } from '@screens/HomeScreen/_components/forecastsGraph/ForecastsGraph.const';
 import CurrentForecastScreenSectionHeader from '@screens/HomeScreen/CurrentForecastScreen/_components/currentForecastScreenSectionHeader/CurrentForecastScreenSectionHeader';
 
 import type { ForecastsGraphSectionWrapperProps } from '@screens/HomeScreen/CurrentForecastScreen/_components/forecastsGraphSection/ForecastsGraphSectionWrapper.type';
 
 const CONTAINER_MARGIN = 20;
+
+/**
+ * 양 옆 그라디언트 컴포넌트
+ * @param reverse 좌우 반전하는지 여부
+ * @jinhok96 25.06.07
+ */
+function GradientOverlay({ className, reverse }: { className: string; reverse?: boolean }) {
+  const backgroundColor = useGetColorHex('--color-background-02');
+
+  // 양 옆 투명도 그라디언트; 90% ~ 0%
+  const gradientColors = [`${backgroundColor}E6`, `${backgroundColor}00`];
+
+  return (
+    <LinearGradient
+      className={className}
+      colors={gradientColors}
+      start={{ x: !reverse ? 0 : 1, y: 0 }}
+      end={{ x: !reverse ? 1 : 0, y: 0 }}
+      style={{ width: CONTAINER_MARGIN }}
+      pointerEvents="none"
+    />
+  );
+}
 
 /**
  * 날씨 그래프 섹션 래퍼
@@ -44,11 +70,17 @@ export default function ForecastsGraphSectionWrapper({
   return (
     <View
       {...props}
+      className={`relative overflow-hidden ${props.className}`}
       onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
     >
       <Show when={!!headerText}>
         <CurrentForecastScreenSectionHeader text={headerText!} />
       </Show>
+      <GradientOverlay className="absolute left-0 top-0 z-10 h-full" />
+      <GradientOverlay
+        className="absolute right-0 top-0 z-10 h-full"
+        reverse
+      />
       <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
