508a447390a9e657525f27bbbf3b5a6aae2df3c5
jinhok96 <jinhok96a@gmail.com>
2025-06-16 17:42:41 +0900

    refactor: 렌더링 최적화를 위해 memo 	modified:   src/screens/HomeScreen/_components/weatherDetailCard/WeatherDetailCard.tsx

--- src/screens/HomeScreen/_components/weatherDetailCard/WeatherDetailCard.tsx
+++ src/screens/HomeScreen/_components/weatherDetailCard/WeatherDetailCard.tsx
@@ -1,4 +1,5 @@
 import type { PropsWithChildren } from 'react';
+import { memo } from 'react';
 import type { PressableProps } from 'react-native';
 import { Pressable, View } from 'react-native';
 
@@ -20,47 +21,50 @@ type WeatherDetailCardProps = Omit<PressableProps, 'children'> &
     mainDataProps: WeatherDetailCardMainDataProps;
   }>;
 
-export default function WeatherDetailCard({
-  type,
-  isSelected,
-  label,
-  mainDataProps,
-  children,
-  className,
-  ...props
-}: WeatherDetailCardProps) {
-  const lang = useSettingStore(state => state.lang);
+export default memo(
+  function WeatherDetailCard({
+    type,
+    isSelected,
+    label,
+    mainDataProps,
+    children,
+    className,
+    ...props
+  }: WeatherDetailCardProps) {
+    const lang = useSettingStore(state => state.lang);
 
-  const containerClassName = classNames('rounded-xl bg-background-02', isSelected && '', !isSelected && '');
+    const containerClassName = classNames('rounded-xl bg-background-02', isSelected && '', !isSelected && '');
 
-  const cardItemContainerClassName = classNames(
-    'flex gap-2 rounded-xl px-4 mx-4 bg-background-03 overflow-hidden transition-[background-color,height,opacity,padding-top,padding-bottom,margin-top,margin-bottom]',
-    !isSelected && 'py-0 mb-0 mt-0 opacity-0 h-0',
-    isSelected && 'py-4 mb-4 mt-1 opacity-100',
-    isSelected && type === 'hourly' && 'h-[14.8rem]', // 7개; py(16*2) + body2(14*1.6)*7 + gap(8)*(7-1) = 236.8px = 14.8rem
-    isSelected && type === 'daily' && 'h-[11rem]', // 5개; py(16*2) + body2(14*1.6)*5 + gap(8)*(5-1) = 176px = 11rem
-  );
+    const cardItemContainerClassName = classNames(
+      'flex gap-2 rounded-xl px-4 mx-4 bg-background-03 overflow-hidden transition-[background-color,height,opacity,padding-top,padding-bottom,margin-top,margin-bottom]',
+      !isSelected && 'py-0 mb-0 mt-0 opacity-0 h-0',
+      isSelected && 'py-4 mb-4 mt-1 opacity-100',
+      isSelected && type === 'hourly' && 'h-[14.8rem]', // 7개; py(16*2) + body2(14*1.6)*7 + gap(8)*(7-1) = 236.8px = 14.8rem
+      isSelected && type === 'daily' && 'h-[11rem]', // 5개; py(16*2) + body2(14*1.6)*5 + gap(8)*(5-1) = 176px = 11rem
+    );
 
-  return (
-    <View className={className}>
-      <Show when={!!label}>
-        <PretendardText
-          typo="caption-3"
-          className="pb-2 text-text-04"
+    return (
+      <View className={className}>
+        <Show when={!!label}>
+          <PretendardText
+            typo="caption-3"
+            className="pb-2 text-text-04"
+          >
+            {typeof label === 'string' ? label : label?.[lang]}
+          </PretendardText>
+        </Show>
+        <Pressable
+          {...props}
+          className={containerClassName}
         >
-          {typeof label === 'string' ? label : label?.[lang]}
-        </PretendardText>
-      </Show>
-      <Pressable
-        {...props}
-        className={containerClassName}
-      >
-        <WeatherDetailCardMainData
-          {...mainDataProps}
-          isSelected={isSelected}
-        />
-        <View className={cardItemContainerClassName}>{children}</View>
-      </Pressable>
-    </View>
-  );
-}
+          <WeatherDetailCardMainData
+            {...mainDataProps}
+            isSelected={isSelected}
+          />
+          <View className={cardItemContainerClassName}>{children}</View>
+        </Pressable>
+      </View>
+    );
+  },
+  (prev, next) => prev.isSelected === next.isSelected,
+);
