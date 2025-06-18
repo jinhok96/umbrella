dfe64fcd18fee42aa8dee62aa1e48b614845a2a9
jinhok96 <jinhok96a@gmail.com>
2025-06-18 15:56:07 +0900

    chore: 체크리스트 플레이스홀더 추가 	modified:   src/components/emptyContent/EmptyContent.tsx 	modified:   src/screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSection.tsx

--- src/screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSection.tsx
+++ src/screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSection.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import type { ViewProps } from 'react-native';
 import { View } from 'react-native';
 
+import EmptyContent from '@components/emptyContent/EmptyContent';
 import Show from '@components/wrapper/Show';
 import { ChecklistSectionButtonListRow } from '@screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSectionButtonList';
 import ChecklistSectionMessageBox from '@screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSectionMessageBox';
 import CurrentForecastScreenSectionHeader from '@screens/HomeScreen/CurrentForecastScreen/_components/currentForecastScreenSectionHeader/CurrentForecastScreenSectionHeader';
 import { useForecastsStore } from '@store/forecastsStore/useForecastsStore';
+import { useSettingStore } from '@store/settingStore/useSettingStore';
 
 import type { LocalizedText } from '@libs/utils/localize/localize.type';
 import type { ChecklistType } from '@screens/HomeScreen/CurrentForecastScreen/_components/checklistSection/ChecklistSectionButtonWrapper.type';
@@ -17,6 +19,11 @@ const SECTION_HEADER_TEXT: LocalizedText = {
   en: 'All set? Double-check!',
 };
 
+const PLACEHOLDER_TEXT: LocalizedText = {
+  ko: '오늘은 가볍게 외출하세요!',
+  en: 'Have a light and easy day out today!',
+};
+
 type ChecklistSection = Omit<ViewProps, 'className'>;
 
 const BUTTON_GAP = 12;
@@ -25,11 +32,12 @@ const BUTTON_MAX_ROW_ITEM_NUM = 4;
 
 /**
  * 체크리스트 섹션 컴포넌트
- * @jinhok96 25.06.12
+ * @jinhok96 25.06.18
  */
 export default function ChecklistSection({ ...props }: ChecklistSection) {
+  const lang = useSettingStore(state => state.lang);
   const checklist = useForecastsStore(state => state.checklist);
-  const [checklistArray, setChecklistArray] = useState<
+  const [checklistMatrix, setChecklistMatrix] = useState<
     Array<Partial<ForecastsStoreState['checklist'] & { type: ChecklistType }>>[]
   >([]);
   const [selected, setSelected] = useState<{ type: ChecklistType; row: number } | null>(null);
@@ -37,7 +45,7 @@ export default function ChecklistSection({ ...props }: ChecklistSection) {
 
   // checklist를 4개씩 2차 배열로 분리
   useEffect(() => {
-    if (!checklist) return setChecklistArray([]);
+    if (!checklist) return setChecklistMatrix([]);
 
     const newChecklist: Array<Partial<ForecastsStoreState['checklist'] & { type: ChecklistType }>> = [];
 
@@ -67,50 +75,55 @@ export default function ChecklistSection({ ...props }: ChecklistSection) {
       newChecklistMatrix[newChecklistMatrix.length - 1].push(item);
     });
 
-    setChecklistArray(newChecklistMatrix);
+    setChecklistMatrix(newChecklistMatrix);
   }, [checklist]);
 
   return (
     <View
       {...props}
-      className="rounded-[1.25rem] bg-background-02"
+      className="rounded-[1.25rem] bg-background-02 pb-5"
     >
       <CurrentForecastScreenSectionHeader text={SECTION_HEADER_TEXT} />
-      {checklistArray.map((item, row) => (
-        <View
-          key={`checklist-row-${item.toString()}`}
-          className="w-full pb-5 pt-0"
-        >
+      <Show when={!!checklistMatrix.length}>
+        {checklistMatrix.map((item, row) => (
           <View
-            className="flex w-full flex-row items-center justify-between"
-            style={{
-              maxHeight: buttonMaxHeight,
-              gap: BUTTON_GAP,
-              paddingLeft: BUTTON_CONTAINER_PADDING,
-              paddingRight: BUTTON_CONTAINER_PADDING,
-            }}
-            onLayout={e => {
-              const { width } = e.nativeEvent.layout;
-              if (!width) return;
-              const newMaxHeight = Math.round((width - BUTTON_CONTAINER_PADDING * 2 - BUTTON_GAP * 3) / 4);
-              if (buttonMaxHeight !== newMaxHeight) setButtonMaxHeight(newMaxHeight);
-            }}
+            key={`checklist-row-${item.toString()}`}
+            className="w-full"
           >
-            <Show when={!!buttonMaxHeight}>
-              {item.map(checklistItem => (
-                <ChecklistSectionButtonListRow
-                  key={checklistItem.type}
-                  type={checklistItem.type}
-                  row={row}
-                  selectedType={selected?.type}
-                  onSelectedChange={setSelected}
-                />
-              ))}
-            </Show>
+            <View
+              className="flex w-full flex-row items-center justify-between"
+              style={{
+                maxHeight: buttonMaxHeight,
+                gap: BUTTON_GAP,
+                paddingLeft: BUTTON_CONTAINER_PADDING,
+                paddingRight: BUTTON_CONTAINER_PADDING,
+              }}
+              onLayout={e => {
+                const { width } = e.nativeEvent.layout;
+                if (!width) return;
+                const newMaxHeight = Math.round((width - BUTTON_CONTAINER_PADDING * 2 - BUTTON_GAP * 3) / 4);
+                if (buttonMaxHeight !== newMaxHeight) setButtonMaxHeight(newMaxHeight);
+              }}
+            >
+              <Show when={!!buttonMaxHeight}>
+                {item.map(checklistItem => (
+                  <ChecklistSectionButtonListRow
+                    key={checklistItem.type}
+                    type={checklistItem.type}
+                    row={row}
+                    selectedType={selected?.type}
+                    onSelectedChange={setSelected}
+                  />
+                ))}
+              </Show>
+            </View>
+            <ChecklistSectionMessageBox selected={selected && row === selected?.row ? selected.type : null} />
           </View>
-          <ChecklistSectionMessageBox selected={selected && row === selected?.row ? selected.type : null} />
-        </View>
-      ))}
+        ))}
+      </Show>
+      <Show when={!checklistMatrix.length}>
+        <EmptyContent subTitle={PLACEHOLDER_TEXT[lang]} />
+      </Show>
     </View>
   );
 }
