f61d2229698212dea766a7fd6a681347e37cf931
jinhok96 <jinhok96a@gmail.com>
2025-06-19 15:59:53 +0900

    chore: 높이 여유분 포함해 직접 지정해 스크롤 위치 변화 최소화, renderItem 콜백 메모이징 	modified:   src/screens/HomeScreen/DailyForecastScreen/_components/DailyForecastScreenWeatherDetailCardSection.tsx 	modified:   src/screens/HomeScreen/HourlyForecastScreen/_components/HourlyForecastScreenWeatherDetailCardSection.tsx

--- src/screens/HomeScreen/DailyForecastScreen/_components/DailyForecastScreenWeatherDetailCardSection.tsx
+++ src/screens/HomeScreen/DailyForecastScreen/_components/DailyForecastScreenWeatherDetailCardSection.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, useCallback } from 'react';
 import type { ViewProps } from 'react-native';
 import { FlatList, View } from 'react-native';
 
@@ -22,11 +22,84 @@ export default forwardRef<
   FlatList<NonNullable<ForecastsStoreState['daily']>[number]>,
   DailyForecastScreenWeatherDetailCardSectionProps
 >(function DailyForecastScreenWeatherDetailCardSection(
+  // eslint-disable-next-line react/prop-types
   { selectedIndex, onSelectedIndexChange, ...props }: DailyForecastScreenWeatherDetailCardSectionProps,
   ref,
 ) {
   const daily = useForecastsStore(state => state.daily);
   const lang = useSettingStore(state => state.lang);
+
+  const renderItem = useCallback(
+    ({ item, index }: { item: NonNullable<ForecastsStoreState['daily']>[number]; index: number }) => {
+      const date = new Date(item.dt * 1000);
+
+      // mainDataProps
+      const badgeLabel = formatDateToMMDD(date);
+      const mainValue = `${Math.round(getDailyAvgTemp(item.temp.morn, item.temp.day, item.temp.eve, item.temp.night))}°`;
+      const firstSubLabel = (
+        <View className="size-5 opacity-40">
+          <CheckCircleIcon />
+        </View>
+      );
+      const firstSubValue = `${Math.round(item.temp.min)}° ~ ${Math.round(item.temp.max)}°`;
+      const secondSubLabel = (
+        <View className="size-5 opacity-40">
+          <LocationIcon filled />
+        </View>
+      );
+      const secondSubValue = `${Math.round(item.pop * 100)}%`;
+      const weatherIconId = item.weather[0].icon;
+
+      // itemProps - value
+      const feelsLike = Math.round(
+        getDailyAvgTemp(item.feels_like.morn, item.feels_like.day, item.feels_like.eve, item.feels_like.night),
+      );
+      const humidity = Math.round(item.humidity);
+      const clouds = Math.round(item.clouds);
+      const uvi = `${convertUVIndexToText(item.uvi)[lang]} (${item.uvi})`;
+      const windDeg = convertWindDegToText(item.wind_deg)[lang];
+      const windSpeed = Math.round(item.wind_speed * 10) / 10;
+
+      return (
+        <WeatherDetailCard
+          type="daily"
+          isSelected={selectedIndex === index}
+          onPress={() => onSelectedIndexChange(index)}
+          mainDataProps={{
+            badgeLabel,
+            mainValue,
+            firstSubLabel,
+            firstSubValue,
+            secondSubLabel,
+            secondSubValue,
+            weatherIconId,
+          }}
+        >
+          <WeatherDetailCardItem
+            label={{ ko: '체감온도', en: 'Feels Like' }}
+            value={`${feelsLike}°`}
+          />
+          <WeatherDetailCardItem
+            label={{ ko: '습도', en: 'Humidity' }}
+            value={`${humidity}%`}
+          />
+          <WeatherDetailCardItem
+            label={{ ko: '운량', en: 'Cloudiness' }}
+            value={`${clouds}%`}
+          />
+          <WeatherDetailCardItem
+            label={{ ko: '바람', en: 'Wind' }}
+            value={`${windDeg} ${windSpeed}m/s`}
+          />
+          <WeatherDetailCardItem
+            label={{ ko: '자외선지수', en: 'UV Index' }}
+            value={uvi}
+          />
+        </WeatherDetailCard>
+      );
+    },
+    [selectedIndex, onSelectedIndexChange, lang],
+  );
 
   if (!daily) return <></>;
 
@@ -37,78 +110,11 @@ export default forwardRef<
     >
       <FlatList
         ref={ref}
-        contentContainerClassName="p-5 pb-12 gap-3"
+        contentContainerClassName="p-5 gap-3 h-[42rem]" // 요소 확장 시 스크롤 위치가 변하지 않도록 높이 직접 지정
         data={daily}
         keyExtractor={item => item.dt.toString()}
         initialNumToRender={8}
-        renderItem={({ item, index }) => {
-          const date = new Date(item.dt * 1000);
-
-          // mainDataProps
-          const badgeLabel = formatDateToMMDD(date);
-          const mainValue = `${Math.round(getDailyAvgTemp(item.temp.morn, item.temp.day, item.temp.eve, item.temp.night))}°`;
-          const firstSubLabel = (
-            <View className="size-5 opacity-40">
-              <CheckCircleIcon />
-            </View>
-          );
-          const firstSubValue = `${Math.round(item.temp.min)}° ~ ${Math.round(item.temp.max)}°`;
-          const secondSubLabel = (
-            <View className="size-5 opacity-40">
-              <LocationIcon filled />
-            </View>
-          );
-          const secondSubValue = `${Math.round(item.pop * 100)}%`;
-          const weatherIconId = item.weather[0].icon;
-
-          // itemProps - value
-          const feelsLike = Math.round(
-            getDailyAvgTemp(item.feels_like.morn, item.feels_like.day, item.feels_like.eve, item.feels_like.night),
-          );
-          const humidity = Math.round(item.humidity);
-          const clouds = Math.round(item.clouds);
-          const uvi = `${convertUVIndexToText(item.uvi)[lang]} (${item.uvi})`;
-          const windDeg = convertWindDegToText(item.wind_deg)[lang];
-          const windSpeed = Math.round(item.wind_speed * 10) / 10;
-
-          return (
-            <WeatherDetailCard
-              type="daily"
-              isSelected={selectedIndex === index}
-              onPress={() => onSelectedIndexChange(index)}
-              mainDataProps={{
-                badgeLabel,
-                mainValue,
-                firstSubLabel,
-                firstSubValue,
-                secondSubLabel,
-                secondSubValue,
-                weatherIconId,
-              }}
-            >
-              <WeatherDetailCardItem
-                label={{ ko: '체감온도', en: 'Feels Like' }}
-                value={`${feelsLike}°`}
-              />
-              <WeatherDetailCardItem
-                label={{ ko: '습도', en: 'Humidity' }}
-                value={`${humidity}%`}
-              />
-              <WeatherDetailCardItem
-                label={{ ko: '운량', en: 'Cloudiness' }}
-                value={`${clouds}%`}
-              />
-              <WeatherDetailCardItem
-                label={{ ko: '바람', en: 'Wind' }}
-                value={`${windDeg} ${windSpeed}m/s`}
-              />
-              <WeatherDetailCardItem
-                label={{ ko: '자외선지수', en: 'UV Index' }}
-                value={uvi}
-              />
-            </WeatherDetailCard>
-          );
-        }}
+        renderItem={renderItem}
       />
     </View>
   );
