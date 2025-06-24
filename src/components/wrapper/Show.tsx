0e960e647e92e01541587a37fa5458932b5d1edc
jinhok96 <jinhok96a@gmail.com>
2025-06-25 00:01:57 +0900

    chore: Show가 null을 반환하지 않도록 수정 	modified:   src/components/wrapper/Show.tsx

--- src/components/wrapper/Show.tsx
+++ src/components/wrapper/Show.tsx
@@ -7,10 +7,10 @@ type ShowProps = PropsWithChildren<{ when: boolean; fallback?: ReactNode }>;
  * @param children 렌더링할 요소
  * @param when 렌더링 조건
  * @param fallback when=false일 경우 렌더링할 요소
- * @jinhok96 25.05.14
+ * @jinhok96 25.06.24
  */
 export default function Show({ children, when, fallback }: ShowProps) {
   if (when) return <>{children}</>;
   if (fallback) return <>{fallback}</>;
-  return null;
+  return <></>;
 }
